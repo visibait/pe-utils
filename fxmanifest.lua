8ac4e57548eec39f02891ed7a6c194fc6ce659d0
MSRP <OMGmsebastian@gmail.com>
2021-01-15 06:53:44 -0500

    Added first things

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+description 'Some utilites you can use for your server'
+
+version '0.0.2'
+
+client_scripts {
+    'client/discord_cl.lua',
+}
