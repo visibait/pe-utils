e6ee9a364df661e05fd464ada3b0e19e43589ade
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-01-28 21:32:37 -0500

    Add files via upload

---
+++ client/menu_cl.lua
@@ -0,0 +1,8 @@
+local id = GetPlayerServerId(NetworkGetEntityOwner(PlayerPedId()))
+local name = GetPlayerName(NetworkGetEntityOwner(PlayerPedId()))
+local players = #GetActivePlayers() .. '/' .. tostring(32)
+local servername = '~b~ Project Entity ~w~| ~o~ID: '
+
+Citizen.CreateThread(function()
+    AddTextEntry('FE_THDR_GTAO', servername .. id .. ' ~w~| ~p~Name: ' .. name .. '~w~ | ~g~' .. players)
+end)
