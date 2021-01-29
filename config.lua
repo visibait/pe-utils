cb1f1833666c5e69bc01fe265166a7e83ce0c481
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-01-28 21:34:36 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,9 @@
 
 Config              = {}
-
+-- Functions
 Config.Actions      = false -- Disables actions but increases ms of script
 Config.DropWeapons  = false -- Disables weapon drops from peds
 
+-- Discord Presence
 Config.Discord      = 777055108013752320
 Config.bigimage	    = 'pe-logo'
