61e9584bd0d13b5467caa93a35652bbd60de8db4
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-05-05 20:50:09 +0500

    RFR: Globbing

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,10 +9,8 @@ description 'Some utilites you can use for your server'
 version '0.0.2'
 
 client_scripts {
-    'client/discord_cl.lua',
-    'client/menu_cl.lua',
-    'client/ped_cl.lua',
     'config.lua',
+    'client/*.lua'
 }
 
 server_scripts {
